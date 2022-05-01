1de52b9c96cd84b5036334d6ef0ec6669de0772e
Thomas Lazar <thomas@razal.de>
2022-05-01 19:24:32 +0200

    Initial vue commit with router

---
+++ rando-tde/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/" class="btn btn-primary">Home</RouterLink>
+        <RouterLink to="/about" class="btn btn-secondary">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style></style>
